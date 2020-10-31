93beeff19acb6e2fc8d8404ca4acbb9a663a06b9
noblesaleka <noblesaleka@hotmail.com>
2020-10-31 23:33:14 +0000

    product admin can edit products

--- products/urls.py
+++ products/urls.py
@@ -7,8 +7,8 @@ from django.views.static import serve
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('author/<author_id>', views.author_detail, name='author_detail'),
+    path('author/<author_id>/', views.author_detail, name='author_detail'),
     path('add/', views.add_product, name='add_product'),
-   
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
 
