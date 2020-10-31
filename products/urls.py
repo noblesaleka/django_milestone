f90645769e842118702376696e98ff427a3aee0b
noblesaleka <noblesaleka@hotmail.com>
2020-10-31 23:44:46 +0000

    product admin can delete products

--- products/urls.py
+++ products/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('author/<author_id>/', views.author_detail, name='author_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
 
