b8d21d36e8a1e7f8f78758c8bf38cc5c0b94343d
noblesaleka <noblesaleka@hotmail.com>
2020-10-31 14:34:25 +0000

    added message toasts

--- django_milestone/settings.py
+++ django_milestone/settings.py
@@ -138,6 +138,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SITE_ID=1
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
