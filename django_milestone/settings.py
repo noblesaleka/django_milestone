3ae6403119a4a8959421bfa57eefdcf9302d36a3
noblesaleka <noblesaleka@hotmail.com>
2020-11-01 01:06:52 +0000

    deplayment part 2

--- django_milestone/settings.py
+++ django_milestone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'd9n+!^!toj+*+#7$l0k8h49fk^!-m%ygv+%d9#k^qh6uzej+s!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cassandra-django-milestone', 'localhost']
 
 
 # Application definition
@@ -97,14 +97,19 @@ WSGI_APPLICATION = 'django_milestone.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
